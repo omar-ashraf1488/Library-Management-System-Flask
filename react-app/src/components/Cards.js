9bd79a5a01773d097713bf29162d5075ca13a601
omar ashraf <omar-ashraf1488>
2021-12-20 22:00:58 +0100

    creat Home page and Cards for books

---
+++ react-app/src/components/Cards.js
@@ -0,0 +1,48 @@
+import React from "react";
+import "./Cards.css";
+import CardItem from "./CardItem";
+
+function Cards() {
+  return (
+    <div className="cards">
+      <h1>Check out the new Books</h1>
+      <div className="cards__container">
+        <div className="cards__wrapper">
+          <ul className="cards__items">
+            <CardItem
+              src="images/book2.jpg"
+              text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+              label="Best Seller"
+              path="/book2"
+            />
+
+            <CardItem
+              src="images/book1.jpg"
+              text="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."
+              label="New"
+              path="/book1"
+            />
+          </ul>
+
+          <ul className="cards__items">
+            <CardItem
+              src="images/book3.jpg"
+              text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+              label="New"
+              path="/book3"
+            />
+
+            <CardItem
+              src="images/book1.jpg"
+              text="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."
+              label="Best Seller"
+              path="/book4"
+            />
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Cards;
