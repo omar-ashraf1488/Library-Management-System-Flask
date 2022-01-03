dd431820e0fbc4cd773f2a40800f4a44098fa78e
omar ashraf <omar-ashraf1488>
2022-01-03 17:35:58 +0100

    send the data to flask

--- api.py
+++ api.py
@@ -1,15 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, json
 
 app = Flask(__name__) #?
 
 # Index route, not to meet 404 
 # @ at route decorator 
-@app.route('/api')
+@app.route('/api', methods = ['GET', 'POST'])
 # Define function for that route
 def index():
-    return {
-        'name': "Hello world!"
-    } 
+    request_data = json.loads(request.data)
+
+    return request_data
+
 
 if __name__ == "__main__":
     # If there is any error it will pop up in the web page
